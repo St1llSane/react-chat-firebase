8b25e2356e124e728e72ec02e00e73bcdc47d16b
Alexander Vetrov <notescape262@gmail.com>
2023-02-03 22:03:10 +0200

    working at firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAenTpRpVKVQK21uv_Yvc1NLK_nP-DIIBQ",
-  authDomain: "react-chat-a265a.firebaseapp.com",
-  projectId: "react-chat-a265a",
-  storageBucket: "react-chat-a265a.appspot.com",
-  messagingSenderId: "297333191542",
-  appId: "1:297333191542:web:9b1df3bca3fe6805398057"
-};
+  apiKey: 'AIzaSyAenTpRpVKVQK21uv_Yvc1NLK_nP-DIIBQ',
+  authDomain: 'react-chat-a265a.firebaseapp.com',
+  projectId: 'react-chat-a265a',
+  storageBucket: 'react-chat-a265a.appspot.com',
+  messagingSenderId: '297333191542',
+  appId: '1:297333191542:web:9b1df3bca3fe6805398057',
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
+export const storage = getStorage()
+export const db = getFirestore()
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,62 @@
+import { useState } from 'react'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth, storage, db } from '../firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { doc, setDoc } from 'firebase/firestore'
 import '../styles/c_styles/login.scss'
 
 const Register = () => {
+  const [err, setErr] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const displayName = e.target[0].value
+    console.log(e.target[0].value)
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      const storageRef = ref(storage, displayName)
+
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on(
+        (error) => {
+          setErr(true)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            })
+
+            await setDoc(doc(db, 'users', res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            })
+
+            await setDoc(doc(db, 'userChats', res.user.uid), {})
+          })
+        }
+      )
+    } catch (error) {
+      setErr(true)
+    }
+  }
+
   return (
     <section className="form-container">
       <div className="form-wrapper">
         <h1 className="logo">Still Chat</h1>
         <span className="title">Register</span>
-        <form className="form-wrapper__form">
-          <input type="text" placeholder="display name" />
+        <form className="form-wrapper__form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <input type="file" id="file" />
@@ -19,6 +68,7 @@ const Register = () => {
         </form>
         <p>You have an account? Login</p>
       </div>
+      {err && <span>Произошла ошибка!</span>}
     </section>
   )
 }
